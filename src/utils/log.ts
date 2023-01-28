981d523bd69fa6a3383411744a9533a202d99346
Marviuz <marviuz.gaming@gmail.com>
2023-01-28 08:24:28 +0800

    Another fix (I forgot to save the other file lol)

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,14 +1,14 @@
 import chalk from 'chalk';
 import { now } from './time';
 
-const log = (...str: any | any[]) => console.log(`> ${now} |`, ...str);
+const log = (...str: any | any[]) => console.log(`> ${now()} |`, ...str);
 log.error = (...str: any | any[]) =>
-  console.error(chalk.red(`> ${now} |`, ...str));
+  console.error(chalk.red(`> ${now()} |`, ...str));
 log.warn = (...str: any | any[]) =>
-  console.warn(chalk.yellow(`> ${now} |`, ...str));
+  console.warn(chalk.yellow(`> ${now()} |`, ...str));
 log.success = (...str: any | any[]) =>
-  console.log(chalk.green(`> ${now} |`, ...str));
+  console.log(chalk.green(`> ${now()} |`, ...str));
 log.info = (...str: any | any[]) =>
-  console.info(chalk.blue(`> ${now} |`, ...str));
+  console.info(chalk.blue(`> ${now()} |`, ...str));
 
 export default log;
