c47ce9176d527d3ef2531884ae4ea1b31d733e93
Marviuz <marviuz.gaming@gmail.com>
2023-04-02 19:40:29 +0800

    add a bunch of new stuff

--- src/events/player/Error.ts
+++ src/events/player/Error.ts
@@ -1,5 +1,6 @@
 import { PlayerEvent } from '@/lib/Event';
+import log from '@/utils/logger';
 
 export default new PlayerEvent('error', async (queue, error) => {
-  console.log(queue, error);
+  log.error(queue, error);
 });
