a48825ad4e4372c19aafe8006ce3987fb68dc368
Marviuz <112187144+Marviuz@users.noreply.github.com>
2023-04-11 22:04:21 +0800

    adds skip command (#6)
    * remove todo
    
    * change liscence to mit and update lockfile
    
    * add lint
    
    * edit package.json
    
    * add a bunch of new stuff
    
    * add build dir to .gitignore
    
    * add build scripts and build helpers
    
    * remove dotenv in start
    
    * add play command
    
    * edit description
    
    * add skip command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,4 @@
 export { default as ping } from './ping';
 export { default as play } from './music/play';
 export { default as queue } from './music/queue';
+export { default as skip } from './music/skip';
