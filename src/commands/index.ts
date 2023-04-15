f1b6042498600d66cc9cd38643e2bc4ca8b9213b
Marviuz <marviuz.gaming@gmail.com>
2023-04-15 18:51:14 +0800

    add remove command to index

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,4 @@ export { default as ping } from './ping';
 export { default as play } from './music/play';
 export { default as queue } from './music/queue';
 export { default as skip } from './music/skip';
+export { default as remove } from './music/remove';
