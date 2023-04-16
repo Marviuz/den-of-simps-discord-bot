4291fe5b001e9d48b67de4c13634e63c68effb12
Marviuz <112187144+Marviuz@users.noreply.github.com>
2023-04-16 08:03:31 +0800

    Feat/jump command (#10)
    * add jump command
    
    * require parameter of jump command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,4 +3,5 @@ export { default as play } from './music/play';
 export { default as queue } from './music/queue';
 export { default as skip } from './music/skip';
 export { default as remove } from './music/remove';
+export { default as jump } from './music/jump';
 export { default as shuffle } from './music/shuffle';
---
+++ src/commands/music/jump.ts
@@ -0,0 +1,52 @@
+import { ApplicationCommandOptionType } from 'discord.js';
+
+import { Command } from '@/lib/Command';
+
+enum CommandOptions {
+  TrackNumber = 'track_number',
+}
+
+export default new Command({
+  name: 'jump',
+  description: 'Jump to a song',
+  options: [
+    {
+      name: CommandOptions.TrackNumber,
+      description: 'Skips to this track number',
+      type: ApplicationCommandOptionType.Number,
+      required: true,
+    },
+  ],
+  run: async ({ args, interaction, client }) => {
+    const trackNumber = args.getNumber(CommandOptions.TrackNumber)!;
+
+    const queue = client.player.queues.get(interaction.guildId!);
+    const tracks = queue?.tracks;
+
+    try {
+      const track = tracks?.toArray()[trackNumber - 1];
+
+      if (track) {
+        queue?.node.skipTo(track);
+        return await interaction.reply({
+          content: `Jumping to ${track.title}`,
+          ephemeral: true,
+        });
+      }
+
+      return await interaction.reply({
+        content: 'Invalid track number',
+        ephemeral: true,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        if (interaction.deferred)
+          return await interaction.editReply(error.message);
+
+        return await interaction.reply(error.message);
+      }
+
+      throw error;
+    }
+  },
+});
