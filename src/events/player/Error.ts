958dbb49463f874ad8a237300343e892998561ec
Marviuz <marviuz.gaming@gmail.com>
2023-04-21 10:52:05 +0800

    add error logger to error events

--- src/events/player/Error.ts
+++ src/events/player/Error.ts
@@ -1,7 +1,10 @@
 import { ErrorEmbed } from '@/embeds/Error';
 import { PlayerEvent } from '@/lib/Event';
+import log from '@/utils/logger';
 
 export default new PlayerEvent('error', async (queue, error) => {
+  log.error(error);
+
   await queue.metadata.interaction.channel?.send({
     embeds: [ErrorEmbed(error.message)],
   });
